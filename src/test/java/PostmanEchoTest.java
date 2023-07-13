c499586bc31619902dda9cdb9aa9b1dee115a3f5
Stavinskaya_M <marina.st6277@gmail.com>
2023-07-13 22:28:06 +0600

    EchoTest

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -1,2 +1,24 @@
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 public class PostmanEchoTest {
+    @Test
+    void EchoTest() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("13/07/23")
+                // Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("13/07/23"))
+        ;
+
+    }
 }
